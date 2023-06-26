493b9910a39e98b89c8cdbee0d5f7d996f072988
DavidJohnFlood <125552521+DavidJohnFlood@users.noreply.github.com>
2023-06-27 03:24:38 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,1 @@
-let a=require(`node:http`),b=`localhost`,c=3001,d=a.createServer((q,r)=>r.end(`Hello, Galvanize!`)).listen(c,b,()=>console.log(`Server is running on ${b}:${c}`))
+let a=require(`http`),b=`localhost`,c=3001,d=a.createServer((q,r)=>r.end(`Hello, Galvanize!`)).listen(c,b,()=>console.log(`Server is running on ${b}:${c}`))
